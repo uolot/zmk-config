9263067fbd0b4470b7142d76c1d515645093892c
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-11-15 15:47:25 +0100

    Remove local keymap-drawer

--- keymap-drawer/make.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-keymap -c ../keymap_drawer.config.yaml parse -c 10 -z ../config/cradio.keymap > keymap.yaml
-keymap -c ../keymap_drawer.config.yaml draw keymap.yaml > keymap.svg
-rsvg-convert keymap.svg -o keymap.png -b white
