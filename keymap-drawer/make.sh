723d4e89737082846e6a109d2c769261a9c1d1d0
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-11-15 15:30:00 +0100

    Add keymap-drawer config and run it in CI

--- keymap-drawer/make.sh
+++ keymap-drawer/make.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-keymap parse -c 10 -z ../config/cradio.keymap > keymap.yaml
-keymap draw keymap.yaml > keymap.svg
+keymap -c ../keymap_drawer.config.yaml parse -c 10 -z ../config/cradio.keymap > keymap.yaml
+keymap -c ../keymap_drawer.config.yaml draw keymap.yaml > keymap.svg
 rsvg-convert keymap.svg -o keymap.png -b white
